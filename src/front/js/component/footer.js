4a14cf01cd2e772e700f5b6ae4c10f026536952d
djmenesesl <dmenesesl08@gmail.com>
2022-10-30 15:56:20 +0000

    Footer terminado, hero avanzado

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,94 +1,62 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-  <div className="container-fluid bg-footer mt-5">
-    <footer className="py-5 bg-footer">
-      <div className="row bg-footer justify-content-center">
-        <div className="col-6 col-md-2 mb-3 bg-footer">
-          <h5 className="bg-footer">Información</h5>
-          <ul className="nav flex-column bg-footer">
-            <li className="nav-item mb-2">
-              <a href="#" className="nav-link p-0 text-muted bg-footer">
-                Home
+  <div className="container-fluid mt-5 bg-footer">
+    <footer
+      className="bg-transparent align-items-center align-middle"
+      style={{ paddingTop: "310px" }}
+    >
+      <div className="row  justify-content-center bg-transparent">
+        <div className="col-6 col-md-2 mb-3  bg-transparent">
+          <h5 className=" bg-transparent">Información</h5>
+          <ul className="nav flex-column bg-transparent">
+            <li className="nav-item mb-2 bg-transparent">
+              <a href="#" className="nav-link p-0 text-muted ">
+                Sobre Nosotros
               </a>
             </li>
-            <li className="nav-item mb-2">
-              <a href="#" className="nav-link p-0 text-muted bg-footer">
-                Features
-              </a>
-            </li>
-            <li className="nav-item mb-2">
-              <a href="#" className="nav-link p-0 text-muted bg-footer">
-                Pricing
-              </a>
-            </li>
-          </ul>
-        </div>
-
-        <div className="col-6 col-md-2 mb-3 bg-footer">
-          <h5 className="bg-footer">Section</h5>
-          <ul className="nav flex-column bg-footer">
-            <li className="nav-item mb-2">
-              <a href="#" className="nav-link p-0 text-muted bg-footer">
-                Home
-              </a>
-            </li>
-            <li className="nav-item mb-2">
-              <a href="#" className="nav-link p-0 text-muted bg-footer">
-                Features
-              </a>
-            </li>
-            <li className="nav-item mb-2">
-              <a href="#" className="nav-link p-0 text-muted bg-footer">
-                Pricing
+            <li className="nav-item mb-2 bg-transparent">
+              <a href="#" className="nav-link p-0 text-muted ">
+                Contáctanos
               </a>
             </li>
           </ul>
         </div>
 
-        <div className="col-6 col-md-2 mb-3 bg-footer">
-          <h5 className="bg-footer">Section</h5>
-          <ul className="nav flex-column bg-footer">
-            <li className="nav-item mb-2">
-              <a href="#" className="nav-link p-0 text-muted bg-footer">
-                Home
-              </a>
-            </li>
-            <li className="nav-item mb-2">
-              <a href="#" className="nav-link p-0 text-muted bg-footer">
-                Features
-              </a>
-            </li>
-            <li className="nav-item mb-2">
-              <a href="#" className="nav-link p-0 text-muted bg-footer">
-                Pricing
+        <div className="col-6 col-md-2 mb-3 bg-transparent">
+          <h5 className="bg-transparent">Siguenos en Redes</h5>
+          <ul className="nav flex-column bg-transparent">
+            <li className="ms-3 bg-transparent">
+              <a className="link-dark" href="#">
+                <i className="fa-brands fa-twitter fs-4 bg-transparent"></i>
+                <i className="fa-brands fa-instagram fs-4 bg-transparent ms-3"></i>
+                <i className="fa-brands fa-facebook fs-4 bg-transparent ms-3"></i>
               </a>
             </li>
           </ul>
         </div>
       </div>
 
-      <div className="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top bg-footer">
-        <p className="bg-footer">
-          &copy; 2022 Company, Inc. All rights reserved.
-        </p>
-        <ul className="list-unstyled d-flex bg-footer">
-          <li className="ms-3 bg-footer">
-            <a className="link-dark" href="#">
-              <i className="fa-brands fa-twitter fs-4 bg-footer"></i>
-            </a>
-          </li>
-          <li className="ms-3 bg-footer">
-            <a className="link-dark" href="#">
-              <i className="fa-brands fa-instagram fs-4 bg-footer"></i>
-            </a>
-          </li>
-          <li className="ms-3 bg-footer">
-            <a className="link-dark" href="#">
-              <i className="fa-brands fa-facebook fs-4 bg-footer"></i>
-            </a>
-          </li>
-        </ul>
+      <div className="d-flex flex-column flex-sm-row justify-content-evenly pt-5 border-top bg-transparent">
+        <img
+          src="https://res.cloudinary.com/dz8eyr7mb/image/upload/v1667067119/Animalium/Animalium-logo-blanco_xhxquy.png"
+          alt="..."
+          className="bg-transparent"
+          style={{ width: "150px", height: "72.95px" }}
+        />
+        <div className="col-6 col-md-3 mb-3 bg-transparent">
+          <p className="bg-transparent fw-bold">
+            Animalium-Vip © Todos los derechos reservados.
+          </p>
+          <ul className="nav flex-column bg-transparent">
+            <li className="nav-item bg-transparent">
+              <a href="#" className="nav-link p-0 text-muted text-center ">
+                Hecho en Venezuela con{" "}
+                <i class="fa-regular fa-heart bg-transparent"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </footer>
   </div>
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -14,91 +14,145 @@ export const Home = () => {
   console.log(dates);
 
   return (
-    <div className="container">
-      <div className="container buscador border border-3 ps-5">
-        <div className="row">
-          <div className="col-md-3">
-            <label for="validationCustom04" className="form-label">
-              <strong>Tipo de mascota</strong>
-            </label>
-            <select
-              style={{ fontSize: "15px" }}
-              className="form-select"
-              id="validationDefault04"
-              required
-            >
-              <option selected disabled value="">
-                Please Select...
-              </option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
-              <option>...</option>
-            </select>
-          </div>
-          <div className="col-md-3">
-            <label for="validationCustom04" className="form-label">
-              <strong>Cantidad</strong>
-            </label>
-            <select
-              style={{ fontSize: "15px" }}
-              className="form-select"
-              id="validationDefault04"
-              required
-            >
-              <option selected disabled value="">
-                Please Select...
-              </option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
-              <option>...</option>
-            </select>
-          </div>
-
-          <div className="col-md-6">
-            <label for="validationCustom04" className="form-label">
-              <strong>Fechas</strong>
-            </label>
-            <div className="calendar">
-              <RangePicker
-                className="w-75"
-                style={{ fontSize: "27px", borderRadius: "0.25rem" }}
-                onChange={(values) => {
-                  setDates(
-                    values.map((item) => {
-                      return moment(item).format("DD-MM-YYYY");
-                    })
-                  );
-                }}
-              />
+    <div className="container-fluid p-0">
+      <div className="container-fluid bg-hero">
+        <div
+          className="container bg-transparent mb-5 text-light"
+          id="buscador-tope"
+        >
+          <div className="row pt-5 bg-transparent">
+            <div className="col-7 fs-1 fw-bold bg-transparent">
+              ¡Tu mascota se sentirá tan feliz como en casa!
+            </div>
+            <div className="col-8 fs-4 bg-transparent">
+              Contamos con la red de cuidadores más grande y confiable de toda
+              Venezuela
             </div>
           </div>
         </div>
+        <div className="container buscador border align-items-center justify-content-center align-middle pt-5">
+          <div className="row">
+            <div className="col-md-3">
+              <label for="validationCustom04" className="form-label">
+                <strong>Tipo de mascota</strong>
+              </label>
+              <select
+                style={{ fontSize: "15px" }}
+                className="form-select"
+                id="validationDefault04"
+                required
+              >
+                <option selected disabled value="">
+                  Please Select...
+                </option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+                <option>...</option>
+              </select>
+            </div>
+            <div className="col-md-3">
+              <label for="validationCustom04" className="form-label">
+                <strong>Cantidad</strong>
+              </label>
+              <select
+                style={{ fontSize: "15px" }}
+                className="form-select"
+                id="validationDefault04"
+                required
+              >
+                <option selected disabled value="">
+                  Please Select...
+                </option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+                <option>...</option>
+              </select>
+            </div>
 
-        <div className="row mt-2">
-          <div className="col-md-6">
-            <label for="validationCustom04" className="form-label">
-              <strong>Ubicación</strong>
-            </label>
-            <select className="form-select" id="validationDefault04" required>
-              <option selected disabled value="">
-                Please Select...
-              </option>
-              <option value="1">Caracas</option>
-              <option value="2">Maturín</option>
-              <option value="3">Three</option>
-              <option>...</option>
-            </select>
+            <div className="col-md-6">
+              <label for="validationCustom04" className="form-label">
+                <strong>Fechas</strong>
+              </label>
+              <div className="calendar">
+                <RangePicker
+                  className="w-75"
+                  style={{ fontSize: "27px", borderRadius: "0.25rem" }}
+                  onChange={(values) => {
+                    setDates(
+                      values.map((item) => {
+                        return moment(item).format("DD-MM-YYYY");
+                      })
+                    );
+                  }}
+                />
+              </div>
+            </div>
           </div>
-          <div className="col-md-6 mt-1 pb-1">
-            <button type="button" className="boton-cuidador w-75 p-2 mt-4">
-              <i
-                className="fa-solid fa-magnifying-glass"
-                style={{ color: "white", background: "transparent" }}
-              ></i>{" "}
-              Buscar cuidador
-            </button>
+
+          <div className="row mt-2">
+            <div className="col-md-6">
+              <label for="validationCustom04" className="form-label">
+                <strong>Ubicación</strong>
+              </label>
+              <select className="form-select" id="validationDefault04" required>
+                <option selected disabled value="">
+                  Please Select...
+                </option>
+                <option value="1">Caracas</option>
+                <option value="2">Maturín</option>
+                <option value="3">Three</option>
+                <option>...</option>
+              </select>
+            </div>
+            <div className="col-md-6 mt-1 pb-1">
+              <button type="button" className="boton-cuidador w-75 p-2 mt-4">
+                <i
+                  className="fa-solid fa-magnifying-glass"
+                  style={{ color: "white", background: "transparent" }}
+                ></i>{" "}
+                Buscar cuidador
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="row mt-4">
+        <div className="col-12 text-center fs-2 fw-bold">
+          Las mascotas son parte de nuestra familia
+        </div>
+      </div>
+      <div className="row justify-content-center">
+        <div className="col-9 text-center fs-4 align-items-center justify-content-center mb-3">
+          Brindar una experiencia llena de amor y cuidados es nuestra promesa.
+          Damos tranquilidad y felicidad tanto a mascotas como a sus dueños.
+        </div>
+      </div>
+      <div class="row row-cols-1 row-cols-md-3 g-4 mt-2">
+        <div class="col">
+          <div class="card">
+            <div class="card-body">
+              <h5 className="card-title text-center">Encuentra un cuidador</h5>
+            </div>
+            <i className="fa-solid fa-person-walking-with-cane fs-1 text-center"></i>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title text-center">Reserva</h5>
+            </div>
+            <i class="fa-solid fa-house fs-1 text-center"></i>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title text-center">Y listo!</h5>
+            </div>
+            <i class="fa-solid fa-face-smile-wink fs-1 text-center"></i>
           </div>
         </div>
       </div>
@@ -111,7 +165,7 @@ export const Home = () => {
           backgroundColor: "#623CEA",
           borderRadius: "15px",
           marginTop: "100px",
-          marginBottom: "100px",
+          marginBottom: "80px",
         }}
       >
         <div className="row g-0" style={{ backgroundColor: "#623CEA" }}>
